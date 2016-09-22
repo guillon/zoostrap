3c6f42173786429227c012a2fa0d47b45ae0de97
Christophe Guillon <christophe.guillon@st.com>
2016-09-22 15:03:22 +0200

    Add support for Ubuntu 16.04 on x86_64
    Add image for ZS_DISTRIB_ID=ubuntu, ZS_DISTRIB_RELEASE=16.04
    on x86_64.

--- tests/test001.sh
+++ tests/test001.sh
@@ -35,6 +35,10 @@ env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=14.04 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=16.04 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
 env ZS_DISTRIB_ID=centos ZS_DISTRIB_RELEASE=5 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
