0b04b5bdf8c445b9f8c70e8102487ea05370b41e
Christophe Guillon <christophe.guillon@st.com>
2018-05-03 17:35:37 +0200

    Add support for Ubuntu 18.04 on x86_64
    Add image for ZS_DISTRIB_ID=ubuntu, ZS_DISTRIB_RELEASE=18.04
    on x86_64.

--- tests/test001.sh
+++ tests/test001.sh
@@ -39,6 +39,10 @@ env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=16.04 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
 env ZS_DISTRIB_ID=centos ZS_DISTRIB_RELEASE=5 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
