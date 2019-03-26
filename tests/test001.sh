94b392673f215ecb65517be851126059918b9947
Herve Knochel <herve.knochel@st.com>
2019-03-26 10:59:11 +0100

    Add support for latest Ubuntu point releases 16.04.6 and 18.04.2

--- tests/test001.sh
+++ tests/test001.sh
@@ -59,11 +59,19 @@ env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=16.04.5 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=16.04.6 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
 env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
 env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04.1 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04.2 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
