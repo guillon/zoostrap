21b302b030579bcc6046500dc25b452eb06b264c
Herve Knochel <herve.knochel@st.com>
2019-10-28 08:31:05 +0100

    Add support for latest Ubuntu point release 18.04.3

--- tests/test001.sh
+++ tests/test001.sh
@@ -75,6 +75,10 @@ env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04.2 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04.3 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
 #env ZS_DISTRIB_ID=centos ZS_DISTRIB_RELEASE=5 \
 #    ZS_DISTRIB_PACKAGES="wget" \
 #    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
