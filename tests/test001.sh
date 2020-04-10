15c4016d9b6530063b6ab3e44df44d9a076608fe
Herve Knochel <herve.knochel@st.com>
2020-04-10 16:19:52 +0200

    Add support for Ubuntu 18.04.4 point release

--- tests/test001.sh
+++ tests/test001.sh
@@ -79,6 +79,10 @@ env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04.3 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=18.04.4 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
 #env ZS_DISTRIB_ID=centos ZS_DISTRIB_RELEASE=5 \
 #    ZS_DISTRIB_PACKAGES="wget" \
 #    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
