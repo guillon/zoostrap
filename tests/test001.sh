26a430ff6bacdf901deba2af8c0b5473e21f6f34
Herve KNOCHEL <herve.knochel@st.com>
2022-04-28 13:28:26 +0200

    Add support for Ubuntu 20.04.4 and 22.04 distributions

--- tests/test001.sh
+++ tests/test001.sh
@@ -103,6 +103,14 @@ env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=20.04.3 \
     ZS_DISTRIB_PACKAGES="wget" \
     $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
 
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=20.04.4 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
+env ZS_DISTRIB_ID=ubuntu ZS_DISTRIB_RELEASE=22.04 \
+    ZS_DISTRIB_PACKAGES="wget" \
+    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
+
 #env ZS_DISTRIB_ID=centos ZS_DISTRIB_RELEASE=5 \
 #    ZS_DISTRIB_PACKAGES="wget" \
 #    $ZOOSTRAP rootfs wget -O /dev/null http://www.google.com
